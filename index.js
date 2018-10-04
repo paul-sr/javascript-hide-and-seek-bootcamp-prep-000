955201a04bedf1360405314e9c1a7227ee39f1da
saipaul <saipaul@hotmail.com>
2018-10-04 03:11:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function increaseRankBy(n) {
 }
 function deepestChild() {
   var temp = document.querySelector(`div#grand-node`) 
-  console.log(`first: ` + temp)
+  console.log(`first: ` + temp.innerHTML)
   while (temp.querySelectorAll(`div`).length > 0) {
     temp = document.querySelector(`div`)[0];
     console.log(temp)
