61030b3ebd9f0155b00aad47054ce239af23d08d
saipaul <saipaul@hotmail.com>
2018-10-04 03:20:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function deepestChild() {
   console.log(`first: ` + temp.querySelectorAll(`div`).length)
   var len = parseInt(temp.querySelectorAll(`div`).length)
 
-  console.log(len + `next: ` + temp.querySelectorAll(`div`)[3].innerHTML)
-  return temp.querySelectorAll(`div`)[3]
+  console.log(len + `next: ` + temp.querySelectorAll(`div`)[len-1].innerHTML)
+  return temp.querySelectorAll(`div`)[len-1]
   
 }
