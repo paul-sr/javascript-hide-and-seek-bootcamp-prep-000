54f09a7fde932a77a15c2bc3989e319d2cdd0d51
saipaul <saipaul@hotmail.com>
2018-10-04 03:21:05 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
+function nestedTarget() {
+  return document.querySelector(`#nested`).querySelector(`.target`)
+}
+function increaseRankBy(n) {
+  var list = document.querySelectorAll(`.ranked-list`)
+  for (let i = 0; i<list.length; i++) {
+    list[i].innerHTML = parseInt(list[i]) + 1;
+  }
+  return list
+}
+function deepestChild() {
+  var temp = document.querySelector(`div#grand-node`) 
+  var len = parseInt(temp.querySelectorAll(`div`).length)
+  return temp.querySelectorAll(`div`)[len-1]
+  
+}
