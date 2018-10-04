1f20a1aec4551e3a1bf0fd982b2a0fce945eb0c1
saipaul <saipaul@hotmail.com>
2018-10-04 03:08:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
+function nestedTarget() {
+  return document.querySelector(`#nested`).querySelector(`.target`)
+}
+function increaseRankBy(n) {
+  var list = document.querySelectorAll(`.ranked-list`)
+  for (let i = 0; i<list.length; i++) {
+    list[i].innerHTML = parseInt(list[i]) + 1;
+  }
+  return list
+}
+function deepestChild() {
+  var temp = document.querySelector(`#grand-node`)
+  while (temp.querySelectorAll(`div`).length > 0) {
+    temp = document.querySelector(`div`);
+  }
+  return temp
+}
